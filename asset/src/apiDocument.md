5e340231d971b018a11554b9a501d5e3779411ba
ALR2310 <lethanhan342004@gmail.com>
2023-10-13 14:19:45 +0700

    cập nhật api get product

---
+++ asset/src/apiDocument.md
@@ -0,0 +1,10 @@
+## Lấy danh sách sản phẩm:
+
+#### Sử dụng đường dẫn endpoint sau:
+ http://localhost:2310/api/getProduct
+
+#### Nếu muốn thêm các điều kiện để lấy sản phẩm:
+http://localhost:2310/api/getProduct?col=Id&val=1
+
+- truyền vào giá trị col = với tên bảng trong csdl
+- truyền giá trị cho val = giá trị trong bảng
--- asset/src/controllers/apiController.js
+++ asset/src/controllers/apiController.js
@@ -11,15 +11,42 @@ function returnData(success = false, notice = 'Lấy dữ liệu thất bại', 
 
 module.exports = {
     async getProduct(req, res) {
+
+
         const connection = await db
         try {
-            var sql = 'select * from product where status = "active" order by id desc'
-            const [rows] = await connection.execute(sql)
-            if (rows.length > 0) {
-                res.json(returnData(true, 'Lấy dữ liệu thành công', rows))
+            // nhận điều kiện truy vấn
+            const condition = req.query;
+
+            // Lấy danh sách sản phẩm
+            var sql = 'select * from product order by id desc';
+            const [result, field] = await connection.execute(sql)
+            // Lấy danh sách tên cột
+            const columnNames = field.map(field => field.name);
+
+            if (Object.keys(condition).length > 0) {
+                // nếu có điều kiện truy vấn
+                var column = condition.col;
+                var value = parseInt(condition.val);
+
+                // Kiểm tra giá trị đầu vào
+                if (!columnNames.includes(column)) {
+                    return res.json(returnData(false, 'Tên col không tồn tại trong bảng'));
+                }
+
+                var sql = `select * from product where ${column} = ? order by id desc`;
+                const [rows, Fields] = await connection.execute(sql, [value]);
+
+                if (rows.length > 0) {
+                    return res.json(returnData(true, 'Lấy dữ liệu thành công', rows));
+                }
             } else {
-                res.json(returnData())
+                // nếu không có điều kiện truy vấn, trả về đầy đủ danh sách
+                if (result.length > 0) {
+                    return res.json(returnData(true, 'Lấy dữ liệu thành công', result));
+                }
             }
+            return res.json(returnData());
         } catch (err) {
             console.log('Lỗi truy vấn:', err)
         }
